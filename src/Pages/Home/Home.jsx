c3a519bdfc92ce2d1528312ce353971288d40725
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2024-02-29 22:56:25 +0530

    [setup] new

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,26 +1,16 @@
-import {
-  About,
-  Rules,
-  Hero,
-  Registration,
-  GDSCTeam,
-  Speaker,
-  Sponsor,
-} from "../../Components";
+import { About, Hero, Registration, GDSCTeam } from "../../Components";
 
 import style from "./Home.module.scss";
-
-import Associations from "../../Assets/Association.json";
 
 const Home = ({ aboutRef, rulesRef, teamRef, registerRef, onLoad }) => {
   return (
     <main className={style.home}>
       <Hero rulesRef={rulesRef} registerRef={registerRef} onLoad={onLoad} />
       <About aboutRef={aboutRef} />
-      <Sponsor title="In Association With" data={Associations} />
-      <Speaker />
+      {/* <Sponsor title="In Association With" data={Associations} /> */}
+      {/* <Speaker /> */}
       <Registration registerRef={registerRef} />
-      <Rules rulesRef={rulesRef} />
+      {/* <Rules rulesRef={rulesRef} /> */}
       <GDSCTeam teamRef={teamRef} />
     </main>
   );
