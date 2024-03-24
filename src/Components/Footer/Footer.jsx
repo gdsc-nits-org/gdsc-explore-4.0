ce3b3ebcee87d2129d4a2b72dca8f7cbf592a6cc
POBOROJO <parijatbhatt1@gmail.com>
2024-03-24 13:59:02 +0530

    Updated : New Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,45 +1,42 @@
-import { AiOutlineTwitter, AiFillYoutube, AiFillInstagram } from "react-icons/ai";
 import style from "./Footer.module.scss";
+import Vector from "../BlueVector/Vector";
+import vectorIconLeft from "../../Assets/images/vector-left.png";
+import vectorIconRight from "../../Assets/images/vector-right.png";
+import Icon from "../Icons/Icon";
+import vectorInstaIcon from "../../Assets/images/instagram-icon.png";
+import vectorTwitterIcon from "../../Assets/images/twitter-icon.png";
+import vectorYoutubeIcon from "../../Assets/images/youtube-icon.png";
+import vectorFacebookIcon from "../../Assets/images/facebook-icon.png";
+import vectorGDSCIcon from "../../Assets/images/gdsc-icon.png";
 
 const Footer = () => {
   return (
-    <footer className={style.footer}>
-      <div className={style.mail}>
-        <h1 className={style.heading}>Email us at</h1>
-        <p className={style.subHeading}>exploregdsc@gmail.com</p>
-      </div>
-      <div className={style.graphic}>
-        <a href="mailto:exploregdsc@gmail.com">
-          <img src="/assets/images/email-logo.svg" alt="email" />
-        </a>
-      </div>
-      <div className={style.links}>
-        <h1 className={style.heading}>Follow us on</h1>
-        <div className={style.socials}>
-          <a
-            href="https://twitter.com/exploregdsc"
-            target="_blank"
-            aria-label="twitter link"
-          >
-            <AiOutlineTwitter id={style.twitter} />
-          </a>
-          <a
-            href="https://www.youtube.com/channel/UCMLgnvt5f-GPdpj-mwgvbmw"
-            target="_blank"
-            aria-label="youtube link"
-          >
-            <AiFillYoutube id={style.youtube} />
-          </a>
-          <a
-            href="https://www.instagram.com/exploregdsc/"
-            target="_blank"
-            aria-label="instagram link"
-          >
-            <AiFillInstagram id={style.instagram} />
-          </a>
+    <>
+      <footer className={style.footer}>
+        <Vector className={style.vector} vectorSrc={vectorIconLeft} />
+
+        <div className={style.iconContainer}>
+          <Icon iconSrc={vectorInstaIcon} />
+          <Icon iconSrc={vectorTwitterIcon} />
         </div>
-      </div>
-    </footer>
+
+        <div className={style.gdscContainer}>
+          <Icon iconSrc={vectorGDSCIcon} />
+          <h2>GDSC Explore</h2>
+          <p>Email us at </p>
+          <h2>exploregdsc@gmail.com</h2>
+        </div>
+
+        <div className={style.iconContainer}>
+          <Icon iconSrc={vectorFacebookIcon} />
+          <Icon iconSrc={vectorYoutubeIcon} />
+        </div>
+
+        <Vector className={style.vector} vectorSrc={vectorIconRight} />
+      </footer>
+
+      <p className={style.copyright}>Copyright © 2024. All Rights Reserved</p>
+    </>
   );
 };
 
